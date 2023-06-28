2bc4a729d773c72cf7daeaa5f6cf848ff770837a
Cole Rosner <93330363+crosner13@users.noreply.github.com>
2023-06-28 11:34:11 -0600

    Update GetLadderFuelsRaster.py
    Removed unused stuff

--- GetLadderFuelsRaster.py
+++ GetLadderFuelsRaster.py
@@ -1,7 +1,6 @@
 
 # Libraries
 import os
-import sys
 import arcpy
 from arcpy.sa import * # Might need the image analyst version instead
 
@@ -9,7 +8,7 @@ from arcpy.sa import * # Might need the image analyst version instead
 def getLadderFuelsRaster(PathToSTD, PathToD00, PathToD01, PathToOutLF):
     
     # Slice to grab ID prefixes
-    justIDs = slice(-8)
+    justIDs = slice(-7)
     rasterIDs = [file[justIDs] for file in os.listdir(PathToSTD) if file.endswith("_normal_std.tif")]
     arcpy.AddMessage(f"Found {len(rasterIDs)} ID prefixes.")
 
@@ -24,22 +23,14 @@ def getLadderFuelsRaster(PathToSTD, PathToD00, PathToD01, PathToOutLF):
         if os.path.exists(stdDev) and os.path.exists(cover1_8) and os.path.exists(cover4_8):
             arcpy.AddMessage(f"Required input files found for ID prefix: {id}. Performing calculation.")
             
-            # This block will hopefully catch errors with the raster calculation and log them to the geoprocessing window
-            try:
-                lfRaster = RasterCalculator([cover1_8, stdDev, cover4_8], ["x", "y", "z"], 
-                                            "20.41 + 0.873 * x -1.73 * y - 0.189 * z", "", "")
-                lfRaster.save(os.path.join(PathToOutLF, str(id)+ "LF.tif"))
-                arcpy.AddMessage(f"LF Raster created and saved for {id}.")
-            except arcpy.ExecuteError:
-                arcpy.AddError(arcpy.GetMessages(2))
-            except:
-                e = sys.exc_info()[1]
-                arcpy.AddError(e.args[0])
-
+            lfRaster = RasterCalculator([cover1_8, stdDev, cover4_8], ["x", "y", "z"], 
+                                        "20.41 + 0.873 * x -1.73 * y - 0.189 * z")
+            lfRaster.save(os.path.join(PathToOutLF, str(id)+ "LF.tif"))
+            arcpy.AddMessage(f"LF Raster created and saved for {id}.")
         else:
             arcpy.AddMessage(f"Skipping ID prefix: {id}. It is missing an input")
 
-        arcpy.AddMessage("Done.")
+    arcpy.AddMessage("Done.")
 
 if __name__ == '__main__':
     
@@ -47,7 +38,6 @@ if __name__ == '__main__':
     d00 = arcpy.GetParameterAsText(1) # full path to folder containing COV1_8 Rasters
     d01 = arcpy.GetParameterAsText(2) # full path to folder containing COV4_8 Rasters
     outLF = arcpy.GetParameterAsText(3) # full path to folder containing output LF Rasters
-
 
     getLadderFuelsRaster(PathToSTD = std, PathToD00 = d00, PathToD01 = d01, PathToOutLF = outLF)
 
